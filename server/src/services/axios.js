11afb1f5182ab937337cdce4065b00e64425d5f5
Mark Du <dulongjie@gmail.com>
2022-10-01 22:08:44 -0400

    data from nasaapi

--- server/src/services/axios.js
+++ server/src/services/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
+// query string: https://images-api.nasa.gov/search?q=earth&media_type=image&page=2
+// default "media_type": "image"
 const baseURL ="https://images-api.nasa.gov"
 const axiosNASA=axios.create({baseURL})
 
-export const getSearchImages= async (text,pageNum=1)=>{
+export const getNASAImages= async (text,pageNum=1)=>{
     const res = await axiosNASA.get(`/search?q=${text}&media_type=image&page=${pageNum}`)
     //todo: deal res.status
     return res.data
--- server/src/types/getImages.js
+++ server/src/types/getImages.js
@@ -1,3 +1,5 @@
+import {getNASAImages} from '../services/axios.js'
+
 export default async (root, args, context, info) => {
     // return [
     //     {
@@ -14,11 +16,24 @@ export default async (root, args, context, info) => {
     let imgList=[]
     try {
         if(q){
-            // const collection=await 
+            const collection=await getNASAImages(q,from).then(res=>res.collection)
+            // console.log(collection)
+
+            if(collection&&collection.items?.length>0){
+                //todo: deal items.length<100 when last page
+                imgList=collection.items.map(item=>({
+                    href:item.links?item.links[0]:"",
+                    title: item.data ? item.data[0].title:"",
+                    description: item.data ? item.data[0].description :"",
+                }))
+            }
+
         }
         
     } catch (error) {
         //todo:deal error
         console.log(error)
     }
+
+    return imgList
 }
